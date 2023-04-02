8788e78e64a91f78de88580820be9be655b9cb8b
장정윤 <jeinie9179@naver.com>
2023-04-02 22:14:24 +0900

    Spring Web Form 실습

--- IdeaProjects/helloSpringMVC/src/main/java/kr/ac/hansung/controller/OfferController.java
+++ IdeaProjects/helloSpringMVC/src/main/java/kr/ac/hansung/controller/OfferController.java
@@ -3,11 +3,17 @@ package kr.ac.hansung.controller;
 import kr.ac.hansung.model.Offer;
 import kr.ac.hansung.service.OfferService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
+@Controller
 public class OfferController {
 
     // controller -> service -> dao
@@ -21,5 +27,34 @@ public class OfferController {
         model.addAttribute("id_offers", offers);
 
         return "offers";
+    }
+
+    @GetMapping("/createoffer")
+    public String createOffer(Model model) {
+        model.addAttribute("offer", new Offer());
+
+        return "createoffer";
+    }
+
+    @PostMapping("/docreate")
+    public String doCreate(Model model, @Valid Offer offer, BindingResult bindingResult) {
+        // 사용자가 입력한 값이 제대로 바인딩되는지
+//        System.out.println(offer); // Controller -> Service -> Dao
+
+        if (bindingResult.hasErrors()) {
+            System.out.println("=== Form data does not validated ===");
+
+            List<ObjectError> errors = bindingResult.getAllErrors();
+
+            for (ObjectError error: errors) {
+                System.out.println(error.getDefaultMessage());
+            }
+
+            return "createoffer";
+        }
+
+        offerService.insert(offer);
+
+        return "offercreated";
     }
 }
---
+++ IdeaProjects/helloSpringMVC/target/helloSpring-1.0-SNAPSHOT/WEB-INF/views/offercreated.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: jangjeong-yun
+  Date: 2023/04/02
+  Time: 8:08 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+  ${offer.name} 이 새로운 제안을 하였습니다. <br/>
+
+  <p><a href="${pageContext.request.contextPath}/offers"> Click here to view current offer</a></p>
+</body>
+</html>
