d0ced3698272db5dbb4157dd76c102096dbedaad
장정윤 <jeinie9179@naver.com>
2023-06-03 19:07:43 +0900

    SpringBoot 실습

---
+++ helloSpringBoot/src/main/java/com/example/hellospringboot/conf/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.hellospringboot.conf;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "app")
+public class AppConfig {
+    private String professor;
+    private String course;
+}
--- helloSpringBoot/src/main/java/com/example/hellospringboot/controller/HelloWorldController.java
+++ helloSpringBoot/src/main/java/com/example/hellospringboot/controller/HelloWorldController.java
@@ -1,13 +1,37 @@
 package com.example.hellospringboot.controller;
 
+import com.example.hellospringboot.conf.AppConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloWorldController {
+    /*@Value("${app.professor}")
+    private String professor;
 
-    @GetMapping("/")
-    public String sayHello() {
+    @Value("${app.course}")
+    private String course;*/
+
+    @Autowired
+    private AppConfig appConfig;
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @GetMapping("/say")
+    public String sayHello(Model model) {
+        /*logger.debug("professor name: {}", professor);
+        logger.debug("course name: {}", course);*/
+
+        logger.debug("professor name: {}", appConfig.getProfessor());
+        logger.debug("course name: {}", appConfig.getCourse());
+
+        model.addAttribute("msg", "hello world");
+        model.addAttribute("appConfig", appConfig);
         return "index";
     }
 }
